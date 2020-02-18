ecec4819381a179c6397e52db663e7676155a3c5
c212 <40395648+c212@users.noreply.github.com>
2020-02-18 13:49:39 -0500

    reading numbers

--- lectures/0218/One.java
+++ lectures/0218/One.java
@@ -6,5 +6,11 @@ public class One {
     System.out.print("type: "); 
     String input = leslie.nextLine(); 
     System.out.println("You have typed: " + input); 
+    Scanner derek = new Scanner(input); 
+    while (derek.hasNextDouble()) {
+      double number = derek.nextDouble(); 
+      System.out.println( number ); 
+        
+    }
   }
 }
