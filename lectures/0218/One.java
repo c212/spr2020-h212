273ebd45908856114955d6029305262dd7d1ad3f
c212 <40395648+c212@users.noreply.github.com>
2020-02-18 13:45:13 -0500

    starting point

---
+++ lectures/0218/One.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; 
+
+public class One {
+  public static void main(String[] args) {
+    Scanner leslie = new Scanner(System.in);  
+    System.out.print("type: "); 
+    String input = leslie.nextLine(); 
+    System.out.println("You have typed: " + input); 
+  }
+}
